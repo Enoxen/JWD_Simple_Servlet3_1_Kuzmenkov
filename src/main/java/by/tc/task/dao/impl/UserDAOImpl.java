baa282b0e3525c41f9e4f8e81c5fa45bdf7bd2e6
OSmol <Olga_Smolyakova@outlook.com>
2017-11-23 17:46:57 +0300

    Update UserDAOImpl.java

--- src/main/java/by/tc/task/dao/impl/UserDAOImpl.java
+++ src/main/java/by/tc/task/dao/impl/UserDAOImpl.java
@@ -1,7 +1,7 @@
 package by.tc.task.dao.impl;
 
 import by.tc.task.dao.UserDAO;
-import static by.tc.task.dao.constants.ConnectionConstant.*;
+import static by.tc.task.dao.constants.ConnectionConstant.*;// не надо статического импорта, это не есть гуд
 import static by.tc.task.dao.constants.PreparedStatement.*;
 
 import static  by.tc.task.dao.constants.ResponseFromDb.*;
@@ -9,7 +9,7 @@ import by.tc.task.entity.User;
 import by.tc.task.exception.DAOException;
 import by.tc.task.exception.RequestToDbException;
 
-import java.sql.*;
+import java.sql.*;// не импортируйте классы через *, пожалейте тех, кто это потом читать будет
 
 /**
  * Created by Y50-70 on 12.11.2017.
@@ -21,11 +21,15 @@ public class UserDAOImpl implements UserDAO {
     public User findUser(String name, String surname) throws DAOException {
         User user;
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");// неименованная константная строка - это раз
+            // а два - ты учел, сколько раз этот оператор будет выполняться
+            // т.к. выполняться он должен только один раз за всю жизнь приложения
         }
         catch (ClassNotFoundException e){
             throw new DAOException("problems with db", e);
         }
+        // два try - уже перебор
+        // всю твою логику можно было запилить в одном и не перегружать метод
         try(Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD)){
             user = executeRequest(name, surname, connection);
         }
@@ -33,7 +37,8 @@ public class UserDAOImpl implements UserDAO {
             throw new DAOException("no connection to db", e);
         }
         catch (RequestToDbException e){
-            throw new DAOException(e.getMessage(),e);
+            throw new DAOException(e.getMessage(),e);// ну зачем ты повторно сетишь сообщение, которое уже есть в объекте е, ведь двумя строчками выше все же правильно написал, а тут что - лень было подумать на причиной эксепшена
+            
         }
         return user;
     }
@@ -76,7 +81,9 @@ public class UserDAOImpl implements UserDAO {
                 user.setEmail(result.getString(EMAIL));
             }
         }
-        catch (SQLException e){
+        catch (SQLException e){// ты перехватил SQLException, чтобы выбросить снова SQLException 
+            // да еще в private методе
+            // что пили, когда это писали, я тоже такого хочу
             throw new SQLException("creating entity problems", e);
         }
         return user;
